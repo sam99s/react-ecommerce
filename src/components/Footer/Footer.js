ffefc0917e20378821547ce650c3b457a4311d16
unknown <samucarp99@gmail.com>
2021-09-16 21:32:39 -0300

    cambios en el footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import logo200 from './logo200.png';
+import logoLinkedIn from './logoLinkedIn.png';
+import logoGitHub from './logoGitHub.png';
 import './Footer.scss';
 
 export const Footer = () => {
@@ -17,7 +19,21 @@ export const Footer = () => {
                 <span>Depordev®/Todos los derechos reservados</span>
             </div>
 
-            <div className="col-4">redes Sociales</div>
+            <div className="col-4">
+                <div className="container-fluid d-flex text-center">
+                    <div className="social">
+                        <a href="https://www.linkedin.com/in/sam99s/" target="_blank">
+                            <img className="social-logo" src={logoLinkedIn}/>
+                        </a>                    
+                    </div>
+                    <div className="social">
+                        <a href="https://github.com/sam99s" target="_blank">
+                            <img className="social-logo" src={logoGitHub}/>
+                        </a>                    
+                    </div>
+                    
+                </div>
+            </div>
         </footer>
     )
     
